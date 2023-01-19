98d7a19ce8a72fbff6887a6f4109446bbeb8e6a3
Dan <42077558+dtsprogramming@users.noreply.github.com>
2023-01-18 18:21:05 -0700

    Bug fixes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,8 @@ var images = document.querySelectorAll('.scrolling-images img');
 var current = 0;
 
 function nextImage() {
+  var prev = (current - 1 + images.length) % images.length;
+  images[prev].style.transform = "translateX(0%)";
   images[current].style.transform = "translateX(-"+ (current * 100) +"%)";
   current = (current + 1) % images.length;
 }
