9fff8de4d86d9005c8c0174b1745cdf3aeb072cb
Dan <42077558+dtsprogramming@users.noreply.github.com>
2023-01-18 18:13:50 -0700

    Update nextImage() to allow for multiple images

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,9 +2,8 @@ var images = document.querySelectorAll('.scrolling-images img');
 var current = 0;
 
 function nextImage() {
-  images[current].classList.remove('active');
+  images[current].style.transform = "translateX(-"+ (current * 100) +"%)";
   current = (current + 1) % images.length;
-  images[current].classList.add('active');
 }
 
 setInterval(nextImage, 3000);
